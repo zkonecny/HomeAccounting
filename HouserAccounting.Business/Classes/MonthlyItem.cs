53713f6b726436f3fe6555970ae4cbd8cedeea1b
Zdenek Konecny <zkonecny@volny.cz>
2016-01-03 20:18:14 +0100

    repositories interfaces moving

---
+++ HouserAccounting.Business/Classes/MonthlyItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseAccounting.Business.Classes
+{
+    public class MonthlyItem
+    {
+        public int Year { get; set; }
+
+        public int Month { get; set; }
+
+        public int TotalIncomes { get; set; }
+
+        public int TotalExpenditure { get; set; }
+    }
+}
---
+++ HouserAccounting.Business/Services/MonthlyStatisticsService.cs
@@ -0,0 +1,31 @@
+﻿using HouseAccounting.Business.Classes;
+using HouseAccounting.Infrastructure.Repositories.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseAccounting.Business.Services
+{
+    public class MonthlyStatisticsService
+    {
+        private readonly IIncomeRepository incomeRepository;
+        private readonly IExpenditureRepository expenditureRepository;
+
+        public MonthlyStatisticsService(IIncomeRepository incomeRepository, IExpenditureRepository expenditureRepository)
+        {
+            this.incomeRepository = incomeRepository;
+            this.expenditureRepository = expenditureRepository;
+        }
+
+        public IEnumerable<MonthlyItem> GetMonthlyStatistic()
+        {
+            var allIncomes = incomeRepository.GetAll().OrderByDescending(income => income.Created);
+            var allExpenditures = expenditureRepository.GetAll().OrderByDescending(expenditure => expenditure.Created);
+
+            return null;
+
+        }
+    }
+}
